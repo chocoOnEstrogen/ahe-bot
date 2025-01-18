771d0c76285efc3fcbe98b147ff09e63a93454d7
stella~ <choco@choco.rip>
2025-01-18 14:39:58 +0900

    chore(ecosystem.config.js, package.json): enhance PM2 configuration and add new scripts for process management

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,10 @@ module.exports = {
 			autorestart: true,
 			watch: false,
 			max_memory_restart: '1G',
+			error_file: 'logs/err.log',
+			out_file: 'logs/out.log',
+			node_args: '--enable-source-maps',
+			restart_delay: 3000, // 3 seconds
 			env: {
 				NODE_ENV: 'development',
 			},
